758da10d02d021d7ee174189ecd3e988998b2cd7
Anthony-dorcas <dorcasanthony196@gmail.com>
2025-06-18 01:14:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var MySisterInlawBrotherFriend = "maxwel"
+var MyBestFiendYoungerSister = "HAPPINESS"
+let MyNeighbourChurchAunty = "mrs. people"
+let MyMumBestFriendDaughter = "peace"
+var MyUncleSecondWifeSon = "samuel"
+
+var MyBrotherAgeIn2025 = 26;
+let MyFriendPhoneDigit = 11;
+var HerSisterJambScoreIs = 360;
+let MyMotherShoeSize = 39;
+let EveryoneAverageScoreRageIs = 60;
+
+let IsMybrotherCrushMarried = false;
+let DoesMyFriendHaveCar = false;
+let IsMyDogAfriadOfCats = true;
+let IsMyTeacherHappyToday = true;
+let CanMyCousinDriveBus = false;
+
+
+console.log(MySisterInlawBrotherFriend.length);
+console.log(MyBestFiendYoungerSister.toLowerCase());
+console.log(MyNeighbourChurchAunty.includes("ple"));
+console.log(MyMumBestFriendDaughter.endsWith("t"));
+console.log(MyUncleSecondWifeSon.charAt("3"));
+
+
+
+
+
+
